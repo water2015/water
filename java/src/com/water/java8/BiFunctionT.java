a13c35efecbc8b6d360e285392c5fc1fdcc948e6
yl70801 <584873698@qq.com>
2015-12-14 02:45:07 -0500

    a

---
+++ java/src/com/water/java8/BiFunctionT.java
@@ -0,0 +1,25 @@
+package com.water.java8;
+
+import java.util.function.BiFunction;
+
+public class BiFunctionT {
+
+	public static void main(String[] args) {
+		BiFunction<String, String, String> bi = (x, y) -> {
+			return x + y;
+		};
+
+		System.out.println(bi.apply("java2s.com", " tutorial"));
+
+		Calculator calculator = new Calculator();
+		String result = calculator.calc((a, b) -> " : " + (a * b), 3, 5);
+
+		System.out.println(result);
+	}
+}
+
+class Calculator {
+	public String calc(BiFunction<Integer, Integer, String> bi, Integer i1, Integer i2) {
+		return bi.apply(i1, i2);
+	}
+}
---
+++ java/src/com/water/java8/ConsumerT.java
@@ -0,0 +1,11 @@
+package com.water.java8;
+
+import java.util.function.Consumer;
+
+public class ConsumerT {
+
+	public static void main(String[] args) {
+		Consumer<String> c = (x) -> System.out.println(x.toLowerCase());
+		c.andThen(c).accept("Java2s.com");
+	}
+}
