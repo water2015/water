b9d70b303ddfeed14b905359736c7077bb57e651
yl70801 <584873698@qq.com>
2015-12-29 13:45:59 +0800

    a

--- java/src/main/java/com/water/obj/ObjCreator.java
+++ java/src/main/java/com/water/obj/ObjCreator.java
@@ -6,39 +6,38 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
 
-public class CrunchifyObj implements Cloneable, Serializable {
+public class ObjCreator implements Cloneable, Serializable {
 
 	private static final long serialVersionUID = 1L;
 
-	public CrunchifyObj() {
-		System.out.println("Hello! CrunchifyObj() just got created..");
+	public ObjCreator() {
+		System.out.println("Hello! Object Creator() just got created..");
 	}
 
 	@Override
 	protected Object clone() throws CloneNotSupportedException {
-		return (CrunchifyObj) super.clone();
+		return (ObjCreator) super.clone();
 	}
 
 	@SuppressWarnings({ "unused", "resource" })
 	public static void main(String[] args) throws Exception {
 
 		// Create Object1
-		CrunchifyObj object1 = new CrunchifyObj();
+		ObjCreator object1 = new ObjCreator();
 
 		// Create Object2
-		CrunchifyObj object2 = (CrunchifyObj) Class.forName("com.water.obj.CrunchifyObj").newInstance();
+		ObjCreator object2 = (ObjCreator) Class.forName("com.water.obj.ObjCreator").newInstance();
 
 		// Create Object3
-		CrunchifyObj secondObject = new CrunchifyObj();
-		CrunchifyObj object3 = (CrunchifyObj) secondObject.clone();
+		ObjCreator secondObject = new ObjCreator();
+		ObjCreator object3 = (ObjCreator) secondObject.clone();
 
 		// Create Object4
-		Object object4 = CrunchifyObj.class.getClassLoader().loadClass("com.water.obj.CrunchifyObj")
-				.newInstance();
+		Object object4 = ObjCreator.class.getClassLoader().loadClass("com.water.obj.ObjCreator").newInstance();
 
 		// Create Object5
 		// create a new file with an ObjectOutputStream
-		FileOutputStream out = new FileOutputStream("crunchify.txt");
+		FileOutputStream out = new FileOutputStream("obj.creator.txt");
 		ObjectOutputStream oout = new ObjectOutputStream(out);
 
 		// write something in the file
@@ -46,8 +45,8 @@ public class CrunchifyObj implements Cloneable, Serializable {
 		oout.flush();
 
 		// create an ObjectInputStream for the file we created before
-		ObjectInputStream ois = new ObjectInputStream(new FileInputStream("crunchify.txt"));
-		CrunchifyObj object5 = (CrunchifyObj) ois.readObject();
+		ObjectInputStream ois = new ObjectInputStream(new FileInputStream("obj.creator.txt"));
+		ObjCreator object5 = (ObjCreator) ois.readObject();
 
 	}
 }
